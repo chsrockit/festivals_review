d9b035e0da56c19d405f1f6a74d00f2e465cc1a1
Jonathan Abourbih <jon.abourbih@gmail.com>
2011-05-07 15:56:47 +0100

    Scores for shows are working

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,4 +4,41 @@ class Venue < ActiveRecord::Base
   def to_param 
     "#{id}-#{name.gsub(/\W/, '-').downcase}"
   end
+
+  def weight(source)
+    weighting = {"Guardian" => 1.35, "Fest" => 1.15, "ThreeWeeks" => 0.6, "The List" => 0.9}
+    return weighting[source]
+  end
+
+  def rate_show(reviews)
+    scores = []
+
+    for item in reviews
+      scores << item.score.to_f * weight(item.source).to_f
+    end
+
+    top_score = scores.slice(0)
+
+    for item in scores.slice(1 .. scores.length)
+      top_score += item
+    end
+
+    return top_score / scores.length * 20
+  end
+
+  def score
+    shows_number = self.shows.length
+    reviews_number = 0
+    reviews = []
+    
+    self.shows.each do |show|
+      reviews_number += show.reviews.length
+      show.reviews.each do |review|
+        reviews << review
+      end
+    end
+
+    venue_score = (shows_number / 146.0*100*0.5 )+(reviews_number/208.0*100)+(rate_show(reviews))/3
+    return venue_score
+  end
 end
